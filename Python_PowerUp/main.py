1bccfed3dbf92677eeeffdca07198ded56ad6693
juaovitu13 <joao.victorsoares@outlook.com>
2024-05-15 09:10:58 -0300

    Projeto de Automação de cadastro de produtos

---
+++ Python_PowerUp/main.py
@@ -0,0 +1,66 @@
+#Automação de cadastro de produtos
+
+import pyautogui
+import time
+
+pyautogui.PAUSE = 0.5
+
+# abrir o navegador (chrome)
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+
+# entrar no link 
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+time.sleep(3)
+
+# Passo 2: Fazer login
+# selecionar o campo de email
+pyautogui.click(x=591, y=367)
+
+# escrever o seu email
+pyautogui.write("pythonimpressionador@gmail.com")
+pyautogui.press("tab") # passando pro próximo campo
+pyautogui.write("senha")
+pyautogui.click(x=795, y=529) # clique no botao de login
+time.sleep(3)
+
+# Passo 3: Importar a base de produtos pra cadastrar
+import pandas as pd
+
+tabela = pd.read_csv("Jornada_Python\Python_PowerUp\produtos.csv")
+
+print(tabela)
+
+# Passo 4: Cadastrar um produto
+for linha in tabela.index:
+    # clicar no campo de código
+    pyautogui.click(x=527, y=257)
+    # pegar da tabela o valor do campo que a gente quer preencher
+    codigo = tabela.loc[linha, "codigo"]
+    # preencher o campo
+    pyautogui.write(str(codigo))
+    # passar para o proximo campo
+    pyautogui.press("tab")
+    # preencher o campo
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+    obs = tabela.loc[linha, "obs"]
+    if not pd.isna(obs):
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    pyautogui.press("enter") # cadastra o produto (botao enviar)
+    # dar scroll de tudo pra cima
+    pyautogui.scroll(5000)
+    # Passo 5: Repetir o processo de cadastro até o fim
+
---
+++ Python_PowerUp/pegar_posicao.py
@@ -0,0 +1,9 @@
+# Código para pega a posição dos cliques do mouse na tela
+
+import time
+import pyautogui
+
+time.sleep(5)
+print(pyautogui.position())
+
+#pyautogui.scroll(200)
